b393c845d49552d3d035ad4c133e268ce618bf23
言葉 <funaox@gmail.com>
2022-08-02 01:11:03 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,40 @@
-importScripts('https://unpkg.com/@gauntface/logger@3.0.13/build/browser-globals.js');
+importScripts('https://unpkg.com/@gauntface/logger@3.0.13/build/browser-globals.js')
 
-logger.setPrefix('web-push-study/service worker');
+logger.setPrefix('web-push-study/service worker')
 
 self.addEventListener('install', () => {
-	self.skipWaiting();
-});
+	self.skipWaiting()
+})
 
-self.addEventListener('push', function(event) {
-	logger.log('Push message received.');
-	let notificationTitle = 'Hello';
-	const notificationOptions = {
-		body: 'Thanks for sending this push msg.',
-		icon: 'https://simple-push-demo.vercel.app/images/logo-192x192.png',
-		badge: 'https://simple-push-demo.vercel.app/images/badge-72x72.png',
-		tag: 'web-push-study-notification',
-		data: {
-			url: 'https://web.dev/push-notifications-overview/',
-		},
-	};
-
-	if (event.data) {
-		const dataText = event.data.text();
-		notificationTitle = 'Received Payload';
-		notificationOptions.body = `Push data: '${dataText}'`;
+self.addEventListener('push', function (event) {
+	logger.log('Push message received.')
+	const data = {
+		...{
+			title: 'Hello',
+			body: 'Thanks for sending this push msg.',
+			icon: 'https://simple-push-demo.vercel.app/images/logo-192x192.png',
+			badge: 'https://simple-push-demo.vercel.app/images/badge-72x72.png',
+			tag: 'web-push-study-notification',
+			data: {
+				url: 'https://web.dev/push-notifications-overview/',
+			},
+		}, ...event.data.json()
 	}
+	console.log(data)
 
 	event.waitUntil(
-		self.registration.showNotification(
-			notificationTitle,
-			notificationOptions,
-		),
-	);
-});
+		self.registration.showNotification(data.title, data),
+	)
+})
 
-self.addEventListener('notificationclick', function(event) {
-	logger.log('Notification clicked.');
-	event.notification.close();
+self.addEventListener('notificationclick', function (event) {
+	logger.log('Notification clicked.')
+	event.notification.close()
 
-	let clickResponsePromise = Promise.resolve();
+	let clickResponsePromise = Promise.resolve()
 	if (event.notification.data && event.notification.data.url) {
-		clickResponsePromise = clients.openWindow(event.notification.data.url);
+		clickResponsePromise = clients.openWindow(event.notification.data.url)
 	}
 
-	event.waitUntil(clickResponsePromise);
-});
+	event.waitUntil(clickResponsePromise)
+})
